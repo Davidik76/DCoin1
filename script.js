04714d4f4edf4904ccd543b1761d1d4722d94c14
Davidik76 <118114060+Davidik76@users.noreply.github.com>
2024-04-27 20:27:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function changeSize(button) {
+            button.style.transform = 'scale(1.02)';
+            setTimeout(() => {
+                button.style.transform = 'scale(1)';
+            }, 100);
+        }
+
+let clicker_span = 0;
+
+const clicker_image = document.getElementById('clicker_image');
+const clicker_spanDisplay = document.getElementById('clicker_span');
+
+clicker_image.addEventListener('click', function() {
+clicker_span++;
+clicker_spanDisplay.textContent = + clicker_span;
+});
+
+function boost_modal() {
+    var boost_modal = document.getElementById("boost_modal");
+    boost_modal.classList.add("flex");
+}
+
+function showDiv() {
+            var div = document.getElementById("myDiv");
+            div.style.display = "flex"; 
+        }
+
+function hideDiv() {
+            var div = document.getElementById("myDiv");
+            div.style.display = "none"; 
+        }
