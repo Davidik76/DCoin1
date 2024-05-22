81dfacfe18a883179047ddd93c16b00b604da0e8
Davidik76 <118114060+Davidik76@users.noreply.github.com>
2024-05-22 14:18:34 +0300

    Delete DCoin directory

--- DCoin/main.py
+++
@@ -1,7 +0,0 @@
-
-
-
-from aiogram import InlineKeyboardButton, KeyboardButton, WebAppInfo
-
-ikb = InlineKeyboardButton("Перейти", web_app=WebAppInfo('https://<your_domain>'))
-kb = KeyboardButton("Перейти", web_app=WebAppInfo('https://<your_domain>'))
